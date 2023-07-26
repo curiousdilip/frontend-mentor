c2b12700c0e16f8076d6dfc3372a63e128b8cfd6
Dilip Kumar <curiousdilip@gmail.com>
2023-07-26 20:55:33 +0530

    class -> classname

--- site/src/components/navbar.js
+++ site/src/components/navbar.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <nav class="navbar">
-    <h1 class="nav-title">Frontend Projects</h1>
-    <ul class="nav-links">
-        <button class="theme-toggle" href="" id="toggle">
-            <i class="fas" id="themeIcon"></i>
+    <nav className="navbar">
+    <h1 className="nav-title">Frontend Projects</h1>
+    <ul className="nav-links">
+        <button className="theme-toggle" href="" id="toggle">
+            <i className="fas" id="themeIcon"></i>
         </button>
 
-        <a class="github" href="https://github.com/curiousdilip/frontend-mentor" target="_blank" rel="noreferrer">
-            <i class="fab fa-github"></i>
+        <a className="github" href="https://github.com/curiousdilip/frontend-mentor" target="_blank" rel="noreferrer">
+            <i className="fab fa-github"></i>
         </a>
 
-        <a class="twitter" href="https://twitter.com/curiousdilip" target="_blank" rel="noreferrer">
-            <i class="fab fa-twitter"></i>
+        <a className="twitter" href="https://twitter.com/curiousdilip" target="_blank" rel="noreferrer">
+            <i className="fab fa-twitter"></i>
         </a>
-        <a class="linkedin" href="https://linkedin.com/in/curiousdilip" target="_blank" rel="noreferrer">
-            <i class="fab fa-linkedin"></i>
+        <a className="linkedin" href="https://linkedin.com/in/curiousdilip" target="_blank" rel="noreferrer">
+            <i className="fab fa-linkedin"></i>
         </a>
     </ul>
 </nav>
