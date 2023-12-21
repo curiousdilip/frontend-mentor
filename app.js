fc90fc550f36920a52de8c2b6a48970a26f126dc
Dilip Kumar <dilipkumar@Dilips-Mac.local>
2023-12-21 18:51:37 +0530

    script tag divided

---
+++ app.js
@@ -0,0 +1,20 @@
+const themeToggle = document.getElementById("toggle");
+const icon = document.getElementById("themeIcon");
+const target = document.getElementsByTagName("body")[0];
+const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+target.classList.toggle(theme);
+const innerIcon = theme === "dark" ? "fa-sun" : "fa-moon";
+icon.classList.add(innerIcon);
+themeToggle.addEventListener("click", () => {
+    const currentIcon = icon.classList.contains("fa-sun")
+        ? "fa-sun"
+        : "fa-moon";
+    const altIcon = currentIcon === "fa-sun" ? "fa-moon" : "fa-sun";
+
+    target.classList.toggle("dark");
+
+    icon.classList.remove(currentIcon);
+    icon.classList.add(altIcon);
+});
