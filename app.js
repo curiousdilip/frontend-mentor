8c775ecf662b438b9edaab7b3c6e287ae9df1d72
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-05-28 20:20:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
 
 
-function displayProjects(projects) {
-    const projectsList = document.getElementById('card-container');
-    projects.forEach(project => {
-        const projectItem = document.createElement('div');
-        projectItem.className = 'card';
-        projectItem.innerHTML = `
-            <div class="card-img">
-                <img src="${project.image}" alt="">
-            </div>
-            <div class="card-content">
-                <h1 class="card-title">${project.title}</h1>
-                <div class="card-labels">
-                    ${project.label.map(label => `<span class="flair">${label}</span>`).join('')}
-                </div>
-                <div class="card-links">
-                    <a target="_blank" class="live-url-card" href="${project.liveLink}">Live URL</a>
-                    <a target="_blank" class="github-card" href="https://github.com/curiousdilip/frontend-mentor/tree/main${project.repoLink}">GitHub</a>
-                </div>
-            </div>
-        `;
-        projectsList.appendChild(projectItem);
-    });
-}
-
-window.onload = function () {
-    displayProjects(projects);
-};
-
 const themeToggle = document.getElementById("toggle");
 const icon = document.getElementById("themeIcon");
 const target = document.getElementsByTagName("body")[0];
