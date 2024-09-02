97d8471b137d30b7d416d463a477326ff3d72c0f
curiousdilip <curiousdilip@gmail.com>
2024-09-02 15:17:18 +0530

    added index file for intermediate projects

---
+++ intermediate/app.js
@@ -0,0 +1,62 @@
+const projects = [
+    {
+        image:
+            "./space-tourism-website/assets/screenshot.png",
+        title: "Space tourism website",
+        label: ["HTML", "CSS"],
+        liveLink:
+            "./space-tourism-website/",
+        repoLink:
+            "/intermediate/space-tourism-website",
+    }
+  
+];
+
+function displayProjects(projects) {
+    const projectsList = document.getElementById('card-container');
+    projects.forEach(project => {
+        const projectItem = document.createElement('div');
+        projectItem.className = 'card';
+        projectItem.innerHTML = `
+            <div class="card-img">
+                <img src="${project.image}" alt="">
+            </div>
+            <div class="card-content">
+                <h1 class="card-title">${project.title}</h1>
+                <div class="card-labels">
+                    ${project.label.map(label => `<span class="flair">${label}</span>`).join('')}
+                </div>
+                <div class="card-links">
+                    <a target="_blank" class="live-url-card" href="${project.liveLink}">Live URL</a>
+                    <a target="_blank" class="github-card" href="https://github.com/curiousdilip/frontend-mentor/tree/main${project.repoLink}">GitHub</a>
+                </div>
+            </div>
+        `;
+        projectsList.appendChild(projectItem);
+    });
+}
+
+window.onload = function () {
+    displayProjects(projects);
+};
+
+const themeToggle = document.getElementById("toggle");
+const icon = document.getElementById("themeIcon");
+const target = document.getElementsByTagName("body")[0];
+const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+target.classList.toggle(theme);
+const innerIcon = theme === "dark" ? "fa-sun" : "fa-moon";
+icon.classList.add(innerIcon);
+themeToggle.addEventListener("click", () => {
+    const currentIcon = icon.classList.contains("fa-sun")
+        ? "fa-sun"
+        : "fa-moon";
+    const altIcon = currentIcon === "fa-sun" ? "fa-moon" : "fa-sun";
+
+    target.classList.toggle("dark");
+
+    icon.classList.remove(currentIcon);
+    icon.classList.add(altIcon);
+});
